a2940959b4f37a80dc43b7e89ecc6d7bab2702b3
MichalWarunek <michal.warunek@gmail.com>
2020-07-07 17:57:00 +0200

    heroku tweaks

---
+++ app/controllers/pins_controller.rb
@@ -0,0 +1,81 @@
+class PinsController < ApplicationController
+  before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+
+  # GET /pins
+  # GET /pins.json
+  def index
+    @pins = Pin.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 3)
+  end
+
+  # GET /pins/1
+  # GET /pins/1.json
+  def show
+  end
+
+  # GET /pins/new
+  def new
+    @pin = current_user.pins.build
+  end
+
+  # GET /pins/1/edit
+  def edit
+  end
+
+  # POST /pins
+  # POST /pins.json
+  def create
+    @pin = current_user.pins.build(pin_params)
+
+    respond_to do |format|
+      if @pin.save
+        format.html { redirect_to @pin, notice: 'Pin został utworzony' }
+        format.json { render :show, status: :created, location: @pin }
+      else
+        format.html { render :new }
+        format.json { render json: @pin.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /pins/1
+  # PATCH/PUT /pins/1.json
+  def update
+    respond_to do |format|
+      if @pin.update(pin_params)
+        format.html { redirect_to @pin, notice: 'Pin zostal zaaktualizowany' }
+        format.json { render :show, status: :ok, location: @pin }
+      else
+        format.html { render :edit }
+        format.json { render json: @pin.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /pins/1
+  # DELETE /pins/1.json
+  def destroy
+    @pin.destroy
+    respond_to do |format|
+      format.html { redirect_to pins_url, notice: 'Pin został usuniety' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_pin
+      @pin = Pin.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def pin_params
+      params.require(:pin).permit(:description, :image, :name)
+    end
+
+  def correct_user
+    @pin = current_user.pins.find_by(id: params[:id])
+    redirect_to pins_path, notice: "Nie jestes uprawniony do edycji tego pinu" if @pin.nil?
+  end
+end
